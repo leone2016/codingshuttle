ee5bd67f5f45f88907ab259e7bd02d277af9cef5
Leonardo Medina <leoz.31@hotmail.com>
2025-09-26 16:59:43 -0500

    feat(week2): init

---
+++ springbootwebtutorial/src/main/java/com/codingshuttle/springbootwebtutorial/springbootwebtutorial/LeonardoMedinaApplication.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.springbootwebtutorial.springbootwebtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeonardoMedinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeonardoMedinaApplication.class, args);
+	}
+
+}
